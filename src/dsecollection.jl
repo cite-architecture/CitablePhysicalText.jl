84a61987c1926d3e6220566eb89bb53c535749ca
Neel Smith <dnsmith.neel@gmail.com>
2022-02-25 10:57:35 -0500

    Rm obsolete function

--- src/dsecollection.jl
+++ src/dsecollection.jl
@@ -170,14 +170,3 @@ function fromcex(trait::DSECex,
     end
 end
 
-
-"""Parse header of `cexsrc` into URN and label for DSE collection.
-$(SIGNATURES)
-`cexsrc` should be a single `citerelationset` block.
-"""
-function headerinfo(cexsrc::AbstractString; delimiter = "|")
-    cexblock = blocks(cexsrc, "citerelationset")[1]
-    urnkv = split(cexblock.lines[1], delimiter)
-    labelkv = split(cexblock.lines[2], delimiter)
-    (Cite2Urn(urnkv[2]), labelkv[2])
-end
