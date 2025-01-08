a5893afb5863f6c90979f27979ef087a35f78d06
Neel Smith <dnsmith.neel@gmail.com>
2025-01-08 06:39:16 -0500

    Built site for gh-pages

--- v0.12.0/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#The-CitablePhysicalText-module","page":"Home","title":"The CitablePhysicalText module","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation has moved to quartopub.","category":"page"}]
-}
--- versions.js
+++
@@ -1,6 +0,0 @@
-var DOC_VERSIONS = [
-  "stable",
-  "v0.12",
-];
-var DOCUMENTER_NEWEST = "v0.12.0";
-var DOCUMENTER_STABLE = "stable";
